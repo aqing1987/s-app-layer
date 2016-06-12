85af2c3b7ae76c308ea62fdbddcdf8d523f91903
Rex <s20063196@163.com>
2016-06-12 09:23:33 +0800

    add data format test

---
+++ c/puzzle/data-format-test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // 0.000000
+    printf("int 5 as float = %f\n", 5);
+    // 2147483639
+    printf("float 5.01 as int = %d\n", 5.01);
+
+    printf("real (5) = %d\n", 5);
+    printf("real (5.01) = %f\n", 5.01);
+
+    return 0;
+}
