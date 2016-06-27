0658e8eee50dd159a55c86f9f8791d803f48a98d
Rex <s20063196@163.com>
2016-06-27 23:08:38 +0800

    [c] apue: read cmd from stdin and exec with signal catcher

---
+++ c/apue/1-8-read-cmd-from-stdin-and-exec-with-signal-catcher.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define MAXLINE 1024
+
+// typedef void (*sighandler_t)(int);
+void sig_int(int signo)
+{
+    printf("interruptted, signo = %d\n", signo);
+}
+
+int main(int argc, char *argv[])
+{
+    char buf[MAXLINE] = "";
+    pid_t pid;
+    int status = 0;
+
+    // sighandler_t signal(int signum, sighandler_t handler);
+    if (signal(SIGINT, sig_int) == SIG_ERR) {
+        perror("signal");
+        return -1;
+    }
+
+    // print prompt (printf requires %% to print %)
+    printf("%% ");
+
+    // char *fgets(char *s, int size, FILE *stream);
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        if (buf[strlen(buf)-1] == '\n') {
+            // replace newline with null
+            buf[strlen(buf)-1] = '\0';
+        }
+
+        if ((pid = fork()) < 0) {
+            perror("fork");
+            return -1;
+        }
+        else if (pid == 0) { // child process
+            // int execlp(const char *file, const char *arg, ...);
+            if (execlp(buf, buf, NULL) < 0) {
+                printf("couldn't execute %s\n", buf);
+                exit(125);
+            }
+        }
+
+        // parent
+        // pid_t waitpid(pid_t pid, int *status, int options);
+        if ((pid = waitpid(pid, &status, 0)) < 0) {
+            perror("waitpid");
+            return -1;
+        }
+
+        printf("child process exit code = %d\n", WEXITSTATUS(status));
+
+        printf("\n%% ");
+    }
+    return 0;
+}
