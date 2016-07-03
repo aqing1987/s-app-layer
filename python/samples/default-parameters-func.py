4527ef2ccd851f7428a9164f808e30c6b422016f
Rex <s20063196@163.com>
2016-07-03 23:31:01 +0800

    [py] add default parameters function sample

---
+++ python/samples/default-parameters-func.py
@@ -0,0 +1,8 @@
+
+def default_parameters(arg1, arg2=2, arg3=3):
+    print 'arg1 = ', arg1
+    print 'arg2 = ', arg2
+    print 'arg3 = ', arg3
+
+default_parameters(10)
+default_parameters(10, 20)
