be62ea35b88a1324ceaf96bd8886f43a1815714c
Neil <s20063196@163.com>
2016-08-23 23:04:43 +0800

    [c] apue: echo all command-line arguments and all environment strings

---
+++ c/apue/8-9-echo-all-command-line-arguments-and-all-environment-strings.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    char **ptr;
+    extern char **environ;
+
+    // echo all command-line args
+    for (i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    // echo all env strings
+    for (ptr = environ; *ptr != 0; ptr++) {
+        printf("%s\n", *ptr);
+    }
+
+    return 0;
+}
