7c6651c176d90fa6cda8b9361a0a1c55da295d22
Neil <s20063196@163.com>
2016-09-05 23:20:32 +0800

    sh: sysvar print

---
+++ sh/samples/sysvar.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This script explains how the system variable works
+
+echo "The name of this program is $0"
+echo "You've input $# parameters. They are $*"
+echo "And the first one is $1"
+echo "The PID of this program is $$"
+echo "..."
+echo "You've excuted correctly, and the return code is $?"
