7031d8c8f96874d8c2446f1dfd03b371d93f0ab2
Neil <neil.volatile@yahoo.com>
2017-01-12 08:58:38 +0800

    cpp: use numeric test in for loop

---
+++ cpp/cpp-primer-6/num_test.cpp
@@ -0,0 +1,18 @@
+// num_test.cpp -- use numeric test in for loop
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+
+    cout << "Enter the starting countdown value: ";
+    int limit;
+    cin >> limit;
+    int i;
+    for (i = limit; i; i--) { // quits when i is 0
+        cout << "i = " << i << "\n";
+    }
+    cout << "Done now that i = " << i << "\n";
+
+    return 0;
+}
