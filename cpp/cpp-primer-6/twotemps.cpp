3574d96d45a7e4418789f5140b39245b05ead22a
Neil <neil.volatile@yahoo.com>
2017-02-24 09:18:55 +0800

    cpp: using overloaded template functions

---
+++ cpp/cpp-primer-6/twotemps.cpp
@@ -0,0 +1,80 @@
+// twotemps.cpp -- using overloaded template functions
+// As with ordinary overloading, overloaded templates need
+// distinct function signatures.
+// Not all template arguments have to be template parameter types.
+// Template Limitations:
+//   1. a = b; would not be true if type T is a built-in array type
+//   2. if (a > b) would not be true if T is an ordinary structure
+//   In short, it's easy to write a template function that cannot
+//   handle certain types.
+#include <iostream>
+
+template <typename T> // original template
+void Swap(T &a, T &b);
+
+template <typename T> // new template
+void Swap(T *a, T *b, int n);
+
+void Show(int a[]);
+
+const int Lim = 8;
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+
+    int i = 10, j = 20;
+
+    cout << "i, j = " << i << ", " << j << ".\n";
+    cout << "Using compiler-generated int swapper:\n";
+    Swap(i, j); // matches original template
+    cout << "Now i, j = " << i << ", " << j << ".\n";
+
+    int d1[Lim] = {0, 7, 0, 4, 1, 7, 7, 6};
+    int d2[Lim] = {0, 7, 2, 0, 1, 9, 6, 9};
+
+    cout << "Original arrays:\n";
+    Show(d1);
+    Show(d2);
+    Swap(d1, d2, Lim); // matches new template
+    cout << "Swapped arrays:\n";
+    Show(d1);
+    Show(d2);
+
+    return 0;
+}
+
+template <typename T>
+void Swap(T &a, T &b)
+{
+    T temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+void Swap(T a[], T b[], int n)
+{
+    T temp;
+
+    for (int i = 0; i < n; i++) {
+        temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+    }
+}
+
+void Show(int a[])
+{
+    using namespace std;
+
+    cout << a[0] << a[1] << "/";
+    cout << a[2] << a[3] << "/";
+    for (int i = 4; i < Lim; i++) {
+        cout << a[i];
+    }
+
+    cout << endl;
+}
