e53190d58ecd03888cd409c2dc87acadb9eb3bda
Neil <neil.volatile@yahoo.com>
2017-02-25 11:29:25 +0800

    c: add printf format '%*s' puzzle test

---
+++ c/puzzle/printf-format-puzzle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("%*s Type: NULL\r\n", 3, "");
+    printf("%*s Type: NULL2\r\n", 10, "");
+    printf("%*s Type: NULL3\r\n", 20, "");
+
+    return 0;
+}
