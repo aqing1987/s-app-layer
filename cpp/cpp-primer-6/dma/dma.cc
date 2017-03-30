f5c41e9f8b1c9371bfb4763ebd5324f537aae3f0
kevin <jingsi.chen@qq.com>
2017-03-30 10:16:15 +0800

    cpp: an inheritance example with dynamic memory allocation and friends

---
+++ cpp/cpp-primer-6/dma/dma.cc
@@ -0,0 +1,103 @@
+// dma.cc -- dma class methods
+
+#include <cstring>
+
+#include "dma.h"
+
+// baseDMA methods =============================================================
+
+baseDMA::baseDMA(const char* l, int r) {
+  label_ = new char[strlen(l) + 1];
+  std::strcpy(label_, l);
+  rating_ = r;
+}
+
+baseDMA::baseDMA(const baseDMA& rs) {
+  label_ = new char[std::strlen(rs.label_) + 1];
+  std::strcpy(label_, rs.label_);
+  rating_ = rs.rating_;
+}
+
+baseDMA::~baseDMA() {
+  delete [] label_;
+}
+
+baseDMA& baseDMA::operator=(const baseDMA& rs) {
+  if (this == &rs)
+    return *this;
+
+  delete [] label_;
+  label_ = new char[std::strlen(rs.label_) + 1];
+  std::strcpy(label_, rs.label_);
+  rating_ = rs.rating_;
+
+  return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const baseDMA& rs) {
+  os << "Label: " << rs.label_ << std::endl;
+  os << "Rating: " << rs.rating_ << std::endl;
+
+  return os;
+}
+
+// lacksDMA methods =============================================================
+
+lacksDMA::lacksDMA(const char* c, const char* l, int r)
+    : baseDMA(l, r) {
+  std::strncpy(color_, c, COL_LEN - 1);
+  color_[COL_LEN - 1] = '\0';
+}
+
+std::ostream& operator<<(std::ostream& os, const lacksDMA& ls) {
+  os << (const baseDMA&) ls;
+  os << "Color: " << ls.color_ << std::endl;
+
+  return os;
+}
+
+// hasDMA methods ===============================================================
+
+hasDMA::hasDMA(const char* s, const char* l, int r)
+    : baseDMA(l, r) {
+  style_ = new char[std::strlen(s) + 1];
+  std::strcpy(style_, s);
+}
+
+hasDMA::hasDMA(const char* s, const baseDMA& rs)
+    : baseDMA(rs) {
+  style_ = new char[std::strlen(s) + 1];
+  std::strcpy(style_, s);
+}
+
+// invoke base class copy constructor
+hasDMA::hasDMA(const hasDMA& hs)
+    : baseDMA(hs) {
+  style_ = new char[std::strlen(hs.style_) + 1];
+  std::strcpy(style_, hs.style_);
+}
+
+hasDMA::~hasDMA() {
+  delete [] style_;
+}
+
+hasDMA& hasDMA::operator=(const hasDMA& hs) {
+  if (this == &hs)
+    return *this;
+
+  baseDMA::operator=(hs); // copy base portion
+
+  delete [] style_; // prepare for new style_
+  style_ = new char[std::strlen(hs.style_) + 1];
+  std::strcpy(style_, hs.style_);
+
+  return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const hasDMA& hs) {
+  // use a type cast to match operator<<(ostream&, const baseDMA&)
+  os << (const baseDMA&) hs;
+  os << "Style: " << hs.style_ << std::endl;
+
+  return os;
+}
---
+++ cpp/cpp-primer-6/dma/dma.h
@@ -0,0 +1,67 @@
+// dma.h -- inheritance and dynamic memory allocation
+// if a base class uses dynamic memory allocation, and the derived class does
+// not itself use dynamic memory allocation, you needn't take any special steps.
+//
+// When both the base class and the derived class use dynamic memory allocation,
+// the derived-class destructor, copy constructor, and assignment operator all
+// must use their base-class counterparts to handle the base-class component.
+// This common requirement is accomplished three different ways. For a destructor
+// it is done automatically. For a constructor, it is accomplished by invoking
+// the base-class copy constructor in the memory initialization list, or else
+// the default constructor is invoked automatically. For the assignment operator,
+// it is accomplished by using the scope-resolution operator in an explicit call
+// of the base-class assignment operator.
+#ifndef DMA_H_
+#define DMA_H_
+
+#include <iostream>
+
+// Bae Class Using DMA
+class baseDMA {
+ public:
+  baseDMA(const char* l = "null", int r = 0);
+  baseDMA(const baseDMA& rs);
+  virtual ~baseDMA();
+
+  baseDMA& operator=(const baseDMA& rs);
+  friend std::ostream& operator<<(std::ostream& os, const baseDMA& rs);
+
+private:
+  char* label_;
+  int rating_;
+};
+
+// derived class without DMA
+// no destructor needed
+// uses implicit copy constructor
+// uses implicit assignment operator
+class lacksDMA : public baseDMA {
+ public:
+  lacksDMA(const char* c = "blank", const char* l = "null", int r = 0);
+  lacksDMA(const char* c, const baseDMA& rs);
+
+  friend std::ostream& operator<<(std::ostream& os, const lacksDMA& ls);
+
+ private:
+  enum {
+    COL_LEN = 40
+  };
+  char color_[COL_LEN];
+};
+
+// derived class with DMA
+class hasDMA : public baseDMA {
+ public:
+  hasDMA(const char* s = "none", const char* l = "null", int r = 0);
+  hasDMA(const char* s, const baseDMA& rs);
+  hasDMA(const hasDMA& hs);
+  ~hasDMA();
+
+  hasDMA& operator=(const hasDMA& hs);
+  friend std::ostream& operator<<(std::ostream& os, const hasDMA& hs);
+
+ private:
+  char* style_;
+};
+
+#endif /* DMA_H_ */
