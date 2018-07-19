c8ce3392e7de22cd5885b36470908777cd4cae02
Neil <neil.volatile@yahoo.com>
2018-07-19 08:41:41 +0800

    ml: projecting vector

--- udacity/ml-engineer-basic/linear-algebra/vectors/vector.py
+++ udacity/ml-engineer-basic/linear-algebra/vectors/vector.py
@@ -6,6 +6,8 @@ getcontext().prec = 30
 class Vector(object):
 
     CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+    NO_UNIQUE_PARALLEL_COMPONENT_MSG = 'no unique parallel component'
+    NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG = 'no unique orthogonal component'
 
     def __init__(self, coordinates):
         try:
@@ -42,6 +44,27 @@ class Vector(object):
         except ZeroDivisionError:
             raise Exception('Cannot normalize the zero vector')
 
+    def component_parallel_to(self, basis):
+        try:
+            u = basis.normalized()
+            weight = self.dot(u)
+            return u.times_scalar(weight)
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+            else:
+                raise e
+
+    def component_orthogonal_to(self, basis):
+        try:
+            projection = self.component_parallel_to(basis)
+            return self.minus(projection)
+        except Exception as e:
+            if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+                raise Exception(self.NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG)
+            else:
+                raise e
+            
     def is_zero(self, tolerance=1e-10):
         return self.magnitude() < tolerance
 
